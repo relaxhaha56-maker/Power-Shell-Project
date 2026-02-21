f5f9c1eaa64d873cbd4afbc1c2bbb611e7c57b19
relaxhaha56-maker <relaxhaha56@gmail.com>
2026-02-21 13:23:25 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,8 +1,15 @@
 $UserKey = Read-Host "Please enter your License Key"
 $MyHWID = (Get-WmiObject Win32_ComputerSystemProduct).UUID
 
-# 1. Database Connection
-$KeyUrl = "https://raw.githubusercontent.com/relaxhaha56-maker/Power-Shell-Project/main/keys.json"
+# --- ระบบส่ง Log เข้า Discord ---
+$WebhookUrl = "https://ptb.discord.com/api/webhooks/1474652149328904202/5fJKJNDq-idufG0rJsCiuYqbPfaSmVVSurn7d0vJN9fi7f2PrPiSflrT8AaHWFTJC3XC"
+$LogBody = @{
+    content = "🚀 **มีคนพยายามเข้าใช้งาน!**`n🔑 **Key:** $UserKey`n🆔 **HWID:** $MyHWID"
+} | ConvertTo-Json
+Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body $LogBody -ContentType "application/json"
+# ------------------------------
+
+$KeyUrl = "https://raw.githubusercontent.com/relaxhaha56-maker/Power-Shell-Project/refs/heads/main/keys.json"
 $Keys = Invoke-RestMethod -Uri $KeyUrl
 
 if (-not $Keys.PSObject.Properties[$UserKey]) {
@@ -12,13 +19,11 @@ elseif ($Keys.$UserKey -eq "" -or $Keys.$UserKey -eq $MyHWID) {
     Write-Host "Successfully!" -ForegroundColor Green
     Write-Host "Initializing BasX Aim Lock system..." -ForegroundColor Cyan
 
-    # 2. Download and Execute DLL
-    $DllUrl = "https://raw.githubusercontent.com/relaxhaha56-maker/Power-Shell-Project/main/gralloc.blue.dll"
+    $DllUrl = "https://raw.githubusercontent.com/relaxhaha56-maker/Power-Shell-Project/refs/heads/main/gralloc.blue.dll"
     $DllPath = "$env:TEMP\gralloc.blue.dll"
     
     try {
         (New-Object Net.WebClient).DownloadFile($DllUrl, $DllPath)
-        # Change 'regsvr32.exe' to your specific injector if needed
         Start-Process -FilePath "regsvr32.exe" -ArgumentList "/s $DllPath"
         Write-Host "BasX Aim Lock: Active" -ForegroundColor Green
     }
@@ -28,6 +33,5 @@ elseif ($Keys.$UserKey -eq "" -or $Keys.$UserKey -eq $MyHWID) {
 }
 else {
     Write-Host "Invalid HWID!" -ForegroundColor Yellow
-    Write-Host "Your HWID is: $MyHWID" -ForegroundColor White
-    Write-Host "Please send this ID to the admin to lock your license."
+    Write-Host "Your HWID: $MyHWID" -ForegroundColor Gray
 }
